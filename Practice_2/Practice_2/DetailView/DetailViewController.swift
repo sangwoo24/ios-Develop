e457fc717c3d22eb3917cbd5b42e2bef62be3433
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2020-10-15 15:52:21 +0900

    iOS - Animation 공부 코드정리
    - 현상금에 따라 다른 Animation적용

--- Practice_2/Practice_2/DetailView/DetailViewController.swift
+++ Practice_2/Practice_2/DetailView/DetailViewController.swift
@@ -13,34 +13,47 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setView()
-        
-        
         prepareAnimation()
     }
     
-    //animation 'on'
-    override func viewDidAppear(_ animated: Bool) {
-        showAnimation()
-    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        //현상금에 따라 애니메이션 다르게 설정해보기
+        if let bounty = viewModel.bountyInfo{
+            if bounty.bounty >= 10000000{
+                showHighBounty();
+            }else{
+                showAnimation()
+            }
+        }
+    }
+    
+    
+    private func showHighBounty(){
+        UIView.transition(with: image, duration: 0.3, options: .transitionCurlDown, animations: nil, completion: nil)
+        UIView.animate(withDuration: 0.3, delay: 0.2, usingSpringWithDamping: 1.0, initialSpringVelocity: 3, options:.allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5, usingSpringWithDamping: 3.0, initialSpringVelocity: 1, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        nameLabel.alpha = 1
+        bountyLabel.alpha = 1
+    }
+    
+    
     private func prepareAnimation(){
-//        nameLabelCenterX.constant = view.bounds.width
-//        bountyLabelCenterX.constant = view.bounds.width
         nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
         bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
         
         nameLabel.alpha = 0
         bountyLabel.alpha = 0
     }
+    
+    
     private func showAnimation(){
-//        nameLabelCenterX.constant = 0
-//        bountyLabelCenterX.constant = 0
-        
-//        UIView.animate(withDuration: 0.3){
-//            self.view.layoutIfNeeded()
-//            //layout을 다시 해야 할 필요가 있다면 해라
-//        }
-//
         UIView.transition(with: image, duration: 0.3 , options: .transitionFlipFromLeft, animations: nil, completion: nil)
         
         UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {

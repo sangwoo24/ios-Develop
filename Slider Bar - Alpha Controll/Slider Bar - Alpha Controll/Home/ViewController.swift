c93ed4276bd3142f20fff196dd1d024c45e662f0
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2020-11-09 00:10:17 +0900

    Project - 3 : Label Size조절
    - Label Layout 이동 및 Imge로 변경 후 Size 조정해보기

--- Slider Bar - Alpha Controll/Slider Bar - Alpha Controll/Home/ViewController.swift
+++ Slider Bar - Alpha Controll/Slider Bar - Alpha Controll/Home/ViewController.swift
@@ -2,18 +2,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sizeSlider: UISlider!
     @IBOutlet weak var alphaSlider: UISlider!
     @IBOutlet weak var Label: UILabel!
-        
+    let maxMultiple = 2
+    var labelSize : CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelSize = Label.font.pointSize
         Label.alpha = 0.5
     }
     
+    @IBAction func sizeControll(_ sender: UISlider) {
+        let value = CGFloat(sizeSlider.value)
+        var size = labelSize * value * CGFloat(maxMultiple)
+        size = size.rounded(.up)
+        Label.font = Label.font.withSize(size)
+
+    }
     @IBAction func alphaControll(_ sender: UISlider) {
         let value = CGFloat(alphaSlider.value)
         Label.alpha = value
-        
     }
 }
 

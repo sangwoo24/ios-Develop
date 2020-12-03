0d6e08bddda21f2a2d88132890e9f7eaf4d9149a
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2020-12-03 23:46:01 +0900

    Project 10 - SimpleMemo App : DB전까지 구현 완료
    - Long gesture
    
    - Cell EditMode
    
    - Cell 누를 시 MemoView에서 수정 가능.

--- Simple Memo App-Project10/Simple Memo App-Project10/HomeViewController.swift
+++ Simple Memo App-Project10/Simple Memo App-Project10/HomeViewController.swift
@@ -2,14 +2,17 @@ import UIKit
 
 class HomeViewController: UIViewController, Mydelegate{
     @IBOutlet weak var table: UITableView!
-    var list : [Memo] = []
+    var memoList : [Memo] = []
+    var timer : Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         table.delegate = self
         table.dataSource = self
+        setGesture()
     }
+
 
     @IBAction func addMemo(_ sender: Any) {
         guard let memoVC = self.storyboard?.instantiateViewController(withIdentifier: "memo") as? MemoViewController else { return }
@@ -18,22 +21,59 @@ class HomeViewController: UIViewController, Mydelegate{
     }
     
     func appendMemo(memo : Memo){
-        list.append(memo)
-        table.reloadData()
+        memoList.append(memo)
+        reloadData()
+    }
+    
+    func insertMemo(index : Int, memo: Memo) {
+        memoList[index] = memo
+        reloadData()
+    }
+    
+    func reloadData(){
+        self.table.reloadData()
+    }
+    
+    func setGesture(){
+        let press = UILongPressGestureRecognizer()
+        press.addTarget(self, action: #selector(self.pressed(_:)))
+        table.addGestureRecognizer(press)
+    }
+    
+    @objc func pressed(_ gesture : UILongPressGestureRecognizer?){
+        guard let ges = gesture else { return }
+        
+        switch ges.state {
+        case .began:
+            timer?.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(setEditMode), userInfo: nil, repeats: true)
+        case .ended, .cancelled:
+            timer?.invalidate()
+        default:
+            break
+        }
+    }
+    
+    @objc func setEditMode(){
+        table.setEditing(true, animated: true)
+    }
+    @IBAction func editDone(_ sender: Any) {
+        table.setEditing(false, animated: true)
     }
 }
 
 
 extension HomeViewController : UITableViewDataSource, UITableViewDelegate{
     
+    //tableView setting
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return memoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell else { return UITableViewCell() }
-        cell.memoTitle?.text = list[indexPath.row].title
-        cell.memoContents?.text = list[indexPath.row].contents
+        cell.memoTitle?.text = memoList[indexPath.row].title
+        cell.memoContents?.text = memoList[indexPath.row].contents
         
         return cell
     }
@@ -42,6 +82,7 @@ extension HomeViewController : UITableViewDataSource, UITableViewDelegate{
         return 60
     }
     
+    //delete
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
@@ -52,19 +93,32 @@ extension HomeViewController : UITableViewDataSource, UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
-            //delete
+            memoList.remove(at: indexPath.row)
+            self.table.deleteRows(at: [indexPath], with: .right)
         }
     }
     
+    //move
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        //move
+        let memo = memoList.remove(at: sourceIndexPath.row)
+        memoList.insert(memo, at: destinationIndexPath.row)
+    }
+    
+    //cell Click
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let memoVC = self.storyboard?.instantiateViewController(withIdentifier: "memo") as? MemoViewController else { return }
+        memoVC.delegate = self
+        memoVC.cellMemo = memoList[indexPath.row]
+        memoVC.index = indexPath.row
+        self.navigationController?.pushViewController(memoVC, animated: true)
     }
 }
 
 protocol Mydelegate {
     func appendMemo(memo : Memo)
+    func insertMemo(index : Int, memo : Memo)
 }
--- Simple Memo App-Project10/Simple Memo App-Project10/MemoViewController.swift
+++ Simple Memo App-Project10/Simple Memo App-Project10/MemoViewController.swift
@@ -5,7 +5,9 @@ class MemoViewController: UIViewController {
     @IBOutlet weak var titleInput: UITextField!
     @IBOutlet weak var contentsInput: UITextView!
     var delegate : Mydelegate?
-        
+    var cellMemo : Memo?
+    var index : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -15,13 +17,33 @@ class MemoViewController: UIViewController {
     func setupUI(){
         contentsInput.layer.borderWidth = 1.0
         contentsInput.layer.borderColor = UIColor.black.cgColor
+        
+        if let cellMemo = cellMemo{
+            titleInput.text = cellMemo.title
+            contentsInput.text = cellMemo.contents
+        }
     }
     
     @IBAction func complete(_ sender: Any) {
-        let title = titleInput.text ?? "제목없음"
-        let contents = contentsInput.text ?? ""
-        let memo = Memo(title,contents)
-        delegate?.appendMemo(memo: memo)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    func inputMemo(){
+        let title = titleInput.text!
+        let contents = contentsInput.text!
+        let memo = Memo(title,contents)
+        
+        if title != ""{
+            if let index = index{
+                delegate?.insertMemo(index: index, memo: memo)
+            } else{
+                delegate?.appendMemo(memo: memo)
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        inputMemo()
     }
 }

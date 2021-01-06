7821764a52a1d8f3bb1c025bcca3f67c2c446b29
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2021-01-06 15:31:48 +0900

    iOS Repository 파일 정리

---
+++ Swift Basic/Test.playground/Contents.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+enum Ramen : Int{
+    case 신라면 = 1
+    case 진라면
+    case 너구리
+    case 짜빠게티
+    
+    func print() -> String {
+        switch self {
+        case .신라면: return "신라면"
+        case .진라면: return "진라면"
+        case .너구리: return "너구리"
+        case .짜빠게티: return "짜빠게티"
+        }
+    }
+}
+
+// Optional Type
+if let ramen = Ramen(rawValue: 4) {
+    print(ramen)
+}
+
+
+enum Person {
+    case name(String)
+    case id(Int)
+    
+    func print() -> String {
+        switch self {
+        case .id(let num):
+            return "id"
+        case .name(let string):
+            return "name"
+        }
+    }
+}
+
+let sangwoo : Person = .name("상우")
+print(sangwoo.print())
+
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode : Barcode = .upc(1,2,3,4)
+productBarcode = .qrCode("sangwoo")
+
+switch productBarcode {
+case .upc(let first, let second, let third, let forth):
+    print("--> upc: \(first), \(second), \(third), \(forth)")
+case let .qrCode(code):
+    print("--> qrCode: \(code)")
+}
+
+enum Temperature {
+    case celcius(Double)
+    case fernheit(Double)
+    case kelvin(Double)
+    
+    var celciusValue: Double {
+        switch self {
+        case let .celcius(value): return value  // == case .celcius(let value): return value
+        case let .fernheit(value): return (value - 32) * 5 / 9
+        case let .kelvin(value): return value + 273
+        }
+    }
+}
+
+func getTemp(with temp: Temperature) -> Double {
+    let value = temp.celciusValue
+    print(value)
+    return 0
+}
+
+var t : Temperature = .fernheit(42)
+t = .celcius(42)
+getTemp(with: t)
--- Test.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-let num1 = [1,2,3]
-let num2 = [2]
-
-let x = num1.filter({num2.contains($0)})
-print(x)
